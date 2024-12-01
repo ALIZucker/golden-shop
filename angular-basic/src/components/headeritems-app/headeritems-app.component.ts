7510a688de5ff2698d5419f11d7d8225f8e08cbd
ALI_Zucker <zabihiali1998@gmail.com>
2024-12-01 16:19:16 +0330

    init git step 2

--- angular-basic/src/components/headeritems-app/headeritems-app.component.ts
+++ angular-basic/src/components/headeritems-app/headeritems-app.component.ts
@@ -1,37 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 import { MenuItem } from 'primeng/api';
 import { MenubarModule } from 'primeng/menubar';
 import { MenuModule } from 'primeng/menu';
+import {NgStyle} from "@angular/common";
+import {Button} from "primeng/button";
 
 @Component({
   selector: 'app-headeritems-app',
   standalone: true,
-  imports: [MenubarModule,MenuModule],
-
+  imports: [MenubarModule, MenuModule, NgStyle, Button],
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './headeritems-app.component.html',
   styleUrl: './headeritems-app.component.css'
 })
 export class HeaderitemsAppComponent implements OnInit{
   items: MenuItem[] | undefined;
+  backgroundColor: string = 'blue';
+  textColor: string = 'white';
 
   ngOnInit() {
     this.items = [
       {
-        label: 'Home',
+        label: 'دسته بندی',
         icon: 'pi pi-home'
       },
       {
-        label: 'Features',
+        label: 'پرفروش ترین ها',
         icon: 'pi pi-star'
       },
       {
-        label: 'Projects',
+        label: 'پیشنهاد ها و تخفبف ها',
         icon: 'pi pi-search',
 
       },
       {
-        label: 'Contact',
+        label: 'شگفت انگیزها',
+        icon: 'pi pi-envelope'
+      },
+      {
+        label: 'آبا سوالی دارد؟',
+        icon: 'pi pi-envelope'
+      },
+      {
+        label: 'آزمایشگاه کفش',
         icon: 'pi pi-envelope'
       }
     ]
